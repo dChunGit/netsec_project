f78bedbfeb32f64addb72b6d718620d6a1b42331
dchun <dchun@utexas.edu>
2019-12-04 22:08:42 -0600

    Update paper and change ip input to txt file

--- code/networkscanner/networkscanner.py
+++ code/networkscanner/networkscanner.py
@@ -1,11 +1,21 @@
 from scapy.all import send,IP,ICMP, TCP
 import csv
+import socket
 
-ips = ["70.184.71.102", "107.21.240.66", "130.156.111.26", "205.134.187.221", "107.148.154.182",
-       "72.194.233.208"]
-print(len(ips))
+# ips = ["70.184.71.102", "107.21.240.66", "130.156.111.26", "205.134.187.221", "107.148.154.182",
+#        "72.194.233.208"]
+ips = []
+with open("ipaddresses.txt") as ipfile:
+    ips = ipfile.read().splitlines()
+
+print(ips)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+myIP = s.getsockname()[0]
+print(myIP)
+s.close()
 for address in ips:
-  ip = IP(src="10.145.58.118", dst=address)
+  ip = IP(src=myIP, dst=address)
   SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
   send(ip/SYN)
 
@@ -25,8 +35,8 @@ with open('log.txt') as f:
               data.add(server_ip)
       except:
         print(clean_line)
-    print(data)
+    print(sorted(data))
 
-with open('ipaddresses.csv','w') as ipaddresses:
+with open('candidates.csv','w') as ipaddresses:
   wr = csv.writer(ipaddresses, quoting=csv.QUOTE_ALL)
   wr.writerow(data)
