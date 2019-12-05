85cd3190646ceaed2903560cce138a2ce0981cb9
dchun <dchun@utexas.edu>
2019-12-04 19:41:16 -0600

    Output ips to csv

--- code/networkscanner/networkscanner.py
+++ code/networkscanner/networkscanner.py
@@ -1,10 +1,15 @@
 from scapy.all import send,IP,ICMP, TCP
+import csv
 
-ips = ["205.134.187.221", "107.21.240.66"]
+ips = ["70.184.71.102", "107.21.240.66", "130.156.111.26", "205.134.187.221", "107.148.154.182",
+       "72.194.233.208"]
+print(len(ips))
 for address in ips:
   ip = IP(src="10.145.58.118", dst=address)
   SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
   send(ip/SYN)
+
+input("Press Enter to continue...")
 
 with open('log.txt') as f:
     lines = f.read().splitlines()
@@ -20,5 +25,8 @@ with open('log.txt') as f:
               data.add(server_ip)
       except:
         print(clean_line)
-    
     print(data)
+
+with open('ipaddresses.csv','w') as ipaddresses:
+  wr = csv.writer(ipaddresses, quoting=csv.QUOTE_ALL)
+  wr.writerow(data)
