e3f9e852a4f11a41c774cea54653bbab3c2cb39f
dchun <dchun@utexas.edu>
2019-12-13 12:17:55 -0600

    Update OS metadata in results

--- code/networkscanner/networkscanner.py
+++ code/networkscanner/networkscanner.py
@@ -1,30 +1,29 @@
 from scapy.all import send,IP,ICMP, TCP
 import csv
 import socket
+from itertools import groupby 
 
-# ips = ["70.184.71.102", "107.21.240.66", "130.156.111.26", "205.134.187.221", "107.148.154.182",
-#        "72.194.233.208"]
-# ips = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 myIP = s.getsockname()[0]
 print(myIP)
 s.close()
 
-# with open("bluekeepips.txt") as ipfile:
-#   for line in ipfile:
-#     address = line.strip()
-#     print(address)
+with open("bluekeepips.txt") as ipfile:
+  for line in ipfile:
+    address = line.strip()
+    print(address)
 
-#     ip = IP(src=myIP, dst=address)
-#     SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
-#     send(ip/SYN)
+    ip = IP(src=myIP, dst=address)
+    SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
+    send(ip/SYN)
 
-# input("Press Enter to continue...")
+input("Press Enter to continue...")
 
 with open('log.txt') as f:
     lines = f.read().splitlines()
     data = []
+    os_data = []
     for line in lines:
       clean_line = line.split("] ")[1]
       try:
@@ -34,15 +33,14 @@ with open('log.txt') as f:
             server_ip = scanned_address['srv'].split('/')[0]
             if server_ip != myIP:
               data.append(server_ip)
-            # else:
-            #   print(scanned_address)
-          else:
-            print(scanned_address)
+          elif '?' not in scanned_address['os']:
+            os_data.append(scanned_address['os'])
       except:
         print(clean_line)
 
     print(len(data))
-    # print(sorted(data))
+    print(len(os_data))
+    print(sorted(os_data))
 
 with open('candidates.csv','w') as ipaddresses:
   wr = csv.writer(ipaddresses)
