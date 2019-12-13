8f29c251f068daf4072149858d84848559aeb80a
dChunGit <dchun@utexas.edu>
2019-12-13 11:33:48 -0600

    Initial batch of ips

--- code/networkscanner/networkscanner.py
+++ code/networkscanner/networkscanner.py
@@ -4,23 +4,21 @@ import socket
 
 # ips = ["70.184.71.102", "107.21.240.66", "130.156.111.26", "205.134.187.221", "107.148.154.182",
 #        "72.194.233.208"]
-ips = []
-with open("ipaddresses.txt") as ipfile:
-    ips = ipfile.read().splitlines()
-
-print(len(ips))
-
-print(ips)
+# ips = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 myIP = s.getsockname()[0]
 print(myIP)
 s.close()
 
-for address in ips:
-  ip = IP(src=myIP, dst=address)
-  SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
-  send(ip/SYN)
+with open("bluekeepips.txt") as ipfile:
+  for line in ipfile:
+    address = line.strip()
+    print(address)
+
+    ip = IP(src=myIP, dst=address)
+    SYN = TCP(sport=1040, dport=3389, flags="S", seq=12345)
+    send(ip/SYN)
 
 input("Press Enter to continue...")
 
